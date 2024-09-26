ee0666bb8a6adafc29d5f558bb6e311b270c878a
Timothy Cherney <tjcherney@hotmail.com>
2024-09-25 23:00:08 -0700

    fixed bug with checking color, can only use lower nibble, moved sim to function, need to add old sim version as another function to test run speeds

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <conio.h>
 
 char* screen = (char*)(1024);
-unsigned char* color = (unsigned char*)(55296);
+char* color = (char*)(55296);
 
 char* joystick = (char*)(56321);
 
@@ -10,11 +10,19 @@ short neighbor_list[1000];
 short neighbor_list_length;
 char neighbor_values[1000];
 short live_list_length;
-#define block_char 224
-#define empty_char 96
-#define place_color 2
-#define block_color 5
-#define empty_color 0
+char block_char = 224;
+char empty_char = 96;
+char place_color = 2;
+char block_color = 5;
+char empty_color = 0;
+short i;
+unsigned short placement_index;
+char left;
+char right;
+char up;
+char down;
+char fire;
+
 #define EVER \
     ;        \
     ;
@@ -46,16 +54,73 @@ inline void add_neighbor(short indx) {
     neighbor_values[indx]++;
 }
 
+inline void neighbor_list_sim() {
+    for (EVER) {
+        neighbor_list_length = 0;
+        for (i = 0; i < live_list_length; ++i) {
+            if (live_list[i] % 40 == 0) {
+                add_neighbor((live_list[i] + 39));
+                add_neighbor((live_list[i] - 1));
+                add_neighbor((live_list[i] + 79));
+
+                add_neighbor((live_list[i] + 41));
+                add_neighbor((live_list[i] + 1));
+                add_neighbor((live_list[i] - 39));
+            } else if (live_list[i] % 40 == 39) {
+                add_neighbor((live_list[i] - 41));
+                add_neighbor((live_list[i] - 1));
+                add_neighbor((live_list[i] + 39));
+
+                add_neighbor((live_list[i] - 39));
+                add_neighbor((live_list[i] - 79));
+                add_neighbor((live_list[i] + 1));
+            } else {
+                add_neighbor((live_list[i] - 41));
+                add_neighbor((live_list[i] - 39));
+                add_neighbor((live_list[i] - 1));
+                add_neighbor((live_list[i] + 1));
+                add_neighbor((live_list[i] + 39));
+                add_neighbor((live_list[i] + 41));
+            }
+
+            add_neighbor((live_list[i] - 40));
+            add_neighbor((live_list[i] + 40));
+            add_self(live_list[i]);
+        }
+        live_list_length = 0;
+        for (i = 0; i < neighbor_list_length; i++) {
+            if (neighbor_values[neighbor_list[i]] < 2) {
+                screen[neighbor_list[i]] = empty_char;
+                color[neighbor_list[i]] = empty_color;
+            } else if (neighbor_values[neighbor_list[i]] > 3) {
+                screen[neighbor_list[i]] = empty_char;
+                color[neighbor_list[i]] = empty_color;
+            } else if (neighbor_values[neighbor_list[i]] == 3) {
+                screen[neighbor_list[i]] = block_char;
+                color[neighbor_list[i]] = block_color;
+                live_list[live_list_length] = neighbor_list[i];
+                live_list_length++;
+            } else {
+                if (screen[neighbor_list[i]] == block_char) {
+                    live_list[live_list_length] = neighbor_list[i];
+                    live_list_length++;
+                }
+            }
+            neighbor_values[neighbor_list[i]] = 0;
+        }
+        if ((*joystick & 0x10) == 0) {
+            if (fire == 0) {
+                placement_index = 0;
+                fire = 1;
+                break;
+            }
+        } else {
+            fire = 0;
+        }
+    }
+}
+
 int main() {
-    short i;
-
-    unsigned short placement_index;
-    char left;
-    char right;
-    char up;
-    char down;
-    char fire;
-
     *((unsigned char*)53281) = 0;
     clrscr();
     placement_index = 0;
@@ -68,12 +133,12 @@ int main() {
         while (placement_index < 1000) {
             if ((*joystick & 0x01) == 0) {
                 if (up == 0) {
-                    if (color[placement_index] != block_color) {
+                    if ((color[placement_index] & 0x0F) != block_color) {
                         screen[placement_index] = empty_char;
                         color[placement_index] = empty_color;
                     }
                     placement_index -= 40;
-                    if (color[placement_index] != block_color) {
+                    if ((color[placement_index] & 0x0F) != block_color) {
                         screen[placement_index] = block_char;
                         color[placement_index] = place_color;
                     }
@@ -84,12 +149,12 @@ int main() {
             }
             if ((*joystick & 0x02) == 0) {
                 if (down == 0) {
-                    if (color[placement_index] != block_color) {
+                    if ((color[placement_index] & 0x0F) != block_color) {
                         screen[placement_index] = empty_char;
                         color[placement_index] = empty_color;
                     }
                     placement_index += 40;
-                    if (color[placement_index] != block_color) {
+                    if ((color[placement_index] & 0x0F) != block_color) {
                         screen[placement_index] = block_char;
                         color[placement_index] = place_color;
                     }
@@ -100,12 +165,12 @@ int main() {
             }
             if ((*joystick & 0x04) == 0) {
                 if (left == 0) {
-                    if (color[placement_index] != block_color) {
+                    if ((color[placement_index] & 0x0F) != block_color) {
                         screen[placement_index] = empty_char;
                         color[placement_index] = empty_color;
                     }
                     placement_index -= 1;
-                    if (color[placement_index] != block_color) {
+                    if ((color[placement_index] & 0x0F) != block_color) {
                         screen[placement_index] = block_char;
                         color[placement_index] = place_color;
                     }
@@ -116,12 +181,12 @@ int main() {
             }
             if ((*joystick & 0x08) == 0) {
                 if (right == 0) {
-                    if (color[placement_index] != block_color) {
+                    if ((color[placement_index] & 0x0F) != block_color) {
                         screen[placement_index] = empty_char;
                         color[placement_index] = empty_color;
                     }
                     placement_index += 1;
-                    if (color[placement_index] != block_color) {
+                    if ((color[placement_index] & 0x0F) != block_color) {
                         screen[placement_index] = block_char;
                         color[placement_index] = place_color;
                     }
@@ -133,7 +198,7 @@ int main() {
             if ((*joystick & 0x10) == 0) {
                 if (fire == 0) {
                     if (screen[placement_index] == block_char &&
-                        color[placement_index] == block_color) {
+                        (color[placement_index] & 0x0F) == block_color) {
                         screen[placement_index] = empty_char;
                         color[placement_index] = empty_color;
                     } else {
@@ -148,76 +213,14 @@ int main() {
         }
         live_list_length = 0;
         for (i = 0; i < 1000; ++i) {
-            if (screen[i] == block_char && color[i] == block_color) {
+            if (screen[i] == block_char && (color[i] & 0x0F) == block_color) {
                 live_list[live_list_length] = i;
                 live_list_length++;
             }
             neighbor_values[i] = 0;
             neighbor_list[i] = -1;
         }
-        for (EVER) {
-            neighbor_list_length = 0;
-            for (i = 0; i < live_list_length; ++i) {
-                if (live_list[i] % 40 == 0) {
-                    add_neighbor((live_list[i] + 39));
-                    add_neighbor((live_list[i] - 1));
-                    add_neighbor((live_list[i] + 79));
-
-                    add_neighbor((live_list[i] + 41));
-                    add_neighbor((live_list[i] + 1));
-                    add_neighbor((live_list[i] - 39));
-                } else if (live_list[i] % 40 == 39) {
-                    add_neighbor((live_list[i] - 41));
-                    add_neighbor((live_list[i] - 1));
-                    add_neighbor((live_list[i] + 39));
-
-                    add_neighbor((live_list[i] - 39));
-                    add_neighbor((live_list[i] - 79));
-                    add_neighbor((live_list[i] + 1));
-                } else {
-                    add_neighbor((live_list[i] - 41));
-                    add_neighbor((live_list[i] - 39));
-                    add_neighbor((live_list[i] - 1));
-                    add_neighbor((live_list[i] + 1));
-                    add_neighbor((live_list[i] + 39));
-                    add_neighbor((live_list[i] + 41));
-                }
-
-                add_neighbor((live_list[i] - 40));
-                add_neighbor((live_list[i] + 40));
-                add_self(live_list[i]);
-            }
-            live_list_length = 0;
-            for (i = 0; i < neighbor_list_length; i++) {
-                if (neighbor_values[neighbor_list[i]] < 2) {
-                    screen[neighbor_list[i]] = empty_char;
-                    color[neighbor_list[i]] = empty_color;
-                } else if (neighbor_values[neighbor_list[i]] > 3) {
-                    screen[neighbor_list[i]] = empty_char;
-                    color[neighbor_list[i]] = empty_color;
-                } else if (neighbor_values[neighbor_list[i]] == 3) {
-                    screen[neighbor_list[i]] = block_char;
-                    color[neighbor_list[i]] = block_color;
-                    live_list[live_list_length] = neighbor_list[i];
-                    live_list_length++;
-                } else {
-                    if (screen[neighbor_list[i]] == block_char) {
-                        live_list[live_list_length] = neighbor_list[i];
-                        live_list_length++;
-                    }
-                }
-                neighbor_values[neighbor_list[i]] = 0;
-            }
-            if ((*joystick & 0x10) == 0) {
-                if (fire == 0) {
-                    placement_index = 0;
-                    fire = 1;
-                    break;
-                }
-            } else {
-                fire = 0;
-            }
-        }
+        neighbor_list_sim();
     }
 
     return 0;
